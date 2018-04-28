a125d15e110024cabe29a7a3ae711866d9960e5c
fumimi <info@fumimi.jp>
2018-04-28 13:18:30 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleAppHk::Application.config.secret_key_base = '30f0001113e2adafd5b8b16af3df35852a286a0a31d8464cfdd16339df94280dd5be08c6c576307a7b5bb0d00e61a2490568c2694c8843600c3317671d8bfc3d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SampleAppHk::Application.config.session_store :cookie_store, key: '_sample_app_hk_session'
